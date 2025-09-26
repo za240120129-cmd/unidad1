e4cfb89e084e7b7a7752ec0b3ee4769d18cbc9fe
za240120129-cmd <za240120129@zapopan.tecmm.edu.mx>
2025-09-26 13:24:37 -0600

    Create 003_nombrado_de_variables.py
    programacion vr

---
+++ Unidad 1/Unidad 1/003_nombrado_de_variables.py
@@ -0,0 +1,20 @@
+x = 10
+y = "Nombre"
+z = 3.9
+# Válido
+_variable = 10
+vari_able = 20
+variable10 = 30
+variable = 60
+variaBle = 10
+# No válido
+#2variable = 10
+#var-iable = 10
+#var iable = 10
+
+x, y, z = 10, 20, 30
+x = 10
+y = "Nombre"
+
+print(x)
+print(y)
