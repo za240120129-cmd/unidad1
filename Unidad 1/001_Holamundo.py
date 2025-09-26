2f4022a37f12ad122b7f31cda20ff6129c628c98
za240120129-cmd <za240120129@zapopan.tecmm.edu.mx>
2025-09-26 13:23:25 -0600

    Create 001_Holamundo.py
    programacion vr

---
+++ Unidad 1/001_Holamundo.py
@@ -0,0 +1,16 @@
+print("hola mundo")
+var1= 1742
+var2 = 17
+print(var1)
+print(var2)
+print("es igual a")
+print(var1+var2)
+
+if var1 == 1742:
+    print("es igual a 1742")
+else:
+    print("es diferente a 1742")
+
+valor_Decimal = 10.1
+cadena = ("hola")
+print(cadena, valor_Decimal)
